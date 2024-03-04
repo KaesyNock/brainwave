c08158ec546f302e5daec34379e6ea8f25e929c9
Kaesy <kaesynock@gmail.com>
2024-03-04 12:41:54 -0500

    wip

---
+++ src/components/Generating.jsx
@@ -0,0 +1,19 @@
+import { ScrollParallax } from 'react-just-parallax'
+import { loading } from '../assets'
+
+const Generating = ({ className }) => {
+  return (
+    <div className={`flex items-center h-{3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${className || ""}text-base`}>
+        <ScrollParallax>
+        <img
+        className="w-5 h-5 mr-4"
+        src={loading}
+        alt="loading">
+            AI is Generating
+        </img>
+        </ScrollParallax>
+    </div>
+  )
+}
+
+export default Generating
